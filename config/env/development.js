772941b34c1ecbf0d2af349a34e4402ba3c1bdec
jegulsky <jegulsky@gmail.com>
2014-02-12 04:14:04 +0200

    content update

--- config/env/development.js
+++ config/env/development.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-    db: "mongodb://localhost/mean-dev",
+    db: "mongodb://localhost/salsastudio",
     app: {
-        name: "MEAN - A Modern Stack - Development"
+        name: "Salsa Studio"
     },
     facebook: {
         clientID: "APP_ID",
--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -4,10 +4,10 @@ angular.module('mean.system').controller('HeaderController', ['$scope', 'Global'
     $scope.global = Global;
 
     $scope.menu = [{
-        'title': 'Articles',
+        'title': 'Статьи',
         'link': 'articles'
     }, {
-        'title': 'Create New Article',
+        'title': 'Создать новую Статью',
         'link': 'articles/create'
     }];
     
