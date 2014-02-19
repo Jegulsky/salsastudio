127974d76d32ca8f93e2088895c5301b632194d2
Alex <alexj@bekitzur.com>
2014-02-19 23:38:53 +0200

    content update

--- gruntfile.js
+++ gruntfile.js
@@ -105,4 +105,7 @@ module.exports = function(grunt) {
 
     //Test task.
     grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
+
+    //Forever task.
+    grunt.registerTask('forever', ['forever:server:start']);
 };
